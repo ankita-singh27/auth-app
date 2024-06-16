c752ef8e6660b18de5458b5f5f0720abf485a9d5
ankita-singh27 <rajputanki5@gmail.com>
2024-06-16 14:34:58 +0530

    login completed

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
-const {signup} = require("../Controllers/Auth");
+const {signup,login} = require("../Controllers/Auth");
+const {auth, isStudent,isAdmin} = require("../middlewares/auth");
 
 router.post("/signup", signup);
+router.post("/login", login);
+
+//testing protected routes for single middleware
+router.get("/test", auth, (req,res) =>{
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for TESTS',
+    });
+});
+
+//Protected Route
+router.get("/student", auth, isStudent, (req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for Students',
+    });
+} );
+
+router.get("/admin", auth, isAdmin, (req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for Admin',
+    });
+});
+
 
 module.exports = router;
